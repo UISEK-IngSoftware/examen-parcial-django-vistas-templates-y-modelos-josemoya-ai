e0f55b9c531ed170b4eaa82f3a76fcdffafb639c
josemoya-ai <jose.moya@uisek.edu.ec>
2025-11-17 00:12:50 -0500

    Finalizacion Examen Parcial 1

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie (models.Model):
+    title = models.CharField(max_length=30, null=False)
+    gender = models.CharField(max_length=30, null=False)
+    director_name = models.CharField(max_length=30, null=False)
+    year_realase = models.DateField()
+    synopsis = models.TextField()
+    poster = models.ImageField(upload_to='posters/', null=False, blank=True)
+
+    def __str__(self):
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({
+            'movies': movies
+        },
+        request))
+
+def movie_details(request, movie_id):
+        movie = Movie.objects.get(id = movie_id)
+        template = loader.get_template('movie_details.html') 
+        context = {
+            'movie': movie
+        }
+        return HttpResponse(template.render(context, request))
